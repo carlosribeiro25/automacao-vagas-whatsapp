2d318e9108e5c8d2b226b501d30d10c49f42ffa6
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-24 13:54:01 -0300

    feat: add config initial in server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,45 @@
-import { fastify } from 'fastify';
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import ScalarApiReference from '@scalar/fastify-api-reference'
 
 const server = fastify({
-    logger: true
+  logger: true,
+}).withTypeProvider<ZodTypeProvider>()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+})
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Sistema de filtros de vagas do Whatsapp',
+      version: '1.0.0',
+    },
+  },
+
+  transform: jsonSchemaTransform,
+})
+
+server.register(ScalarApiReference, {
+  routePrefix: '/docs',
 })
 
 server.get('/', () => {
-    return 'Hello world!'
+  return 'Hello world!'
 })
 
-server.listen({port: 3333, host: '0.0.0.0'}).then( () => {
-    console.log("HTTP server running http://localhost:3333/ ") 
+server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running http://localhost:3333/ ')
 })
