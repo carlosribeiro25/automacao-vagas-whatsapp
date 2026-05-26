3b3603b0f8b9d5eb5df3ee72cfa4a72fedd4d05b
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-26 00:43:00 -0300

    feat: add route health in backend

---
+++ src/routes/route-default.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from "fastify";
+
+export async function routeDefault(app: FastifyInstance) {
+    app.get('/', (request, reply) => {
+        return reply.status(200).send({ message: 'Nossa api esta em funcionamento'})
+    } )
+}
