e2f216dc702e5ef9fc9ff97cbd20ebe5ae25c617
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-27 02:46:25 -0300

    feat: add vision routes for test endpoint

---
+++ src/modules/vision/vision.routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from "fastify";
+import { testVisionController } from '@/modules/vision/vision.controller.js'
+
+export async function visionRoutes(app: FastifyInstance) {
+    app.post('/vision/test', testVisionController)
+}
+
