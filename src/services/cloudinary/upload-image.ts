48b14cb936e451d82089761ba5260fb645f6862f
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-31 10:21:18 -0300

    feat: implement upload function for cloudinary integration

---
+++ src/services/cloudinary/upload-image.ts
@@ -0,0 +1,8 @@
+import { uploadImagemCloudinary } from "./cloudinary.service.js";
+
+export async function uploadImage(filePath: string) {
+    
+    const secure_url = await uploadImagemCloudinary(filePath)
+
+    return secure_url
+}
