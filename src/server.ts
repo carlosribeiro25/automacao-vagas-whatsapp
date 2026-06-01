0a756c6605f9114df5a4f4ac5652d7bd8f2c86c3
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-01 15:50:46 -0300

    fix: remove whitespace and import order server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import { server } from './app.js'
+import { startProcessor } from './modules/whatsaap/whatsaap.processor.js'
 import { startWhatsappWorker } from './modules/whatsaap/whatsaap.worker.js'
+
 startWhatsappWorker()
+startProcessor()
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running http://localhost:3333/ ')
+  console.log('HTTP server running http://localhost:3333/')
 })
