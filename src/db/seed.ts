2746377a92e44c4e3225d0a82c704427a7102c3d
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-05 14:58:15 -0300

    feat: add seed.te to populate user to make tests

---
+++ src/db/seed.ts
@@ -0,0 +1,20 @@
+import { users } from "./schema.js";
+import { db } from "./index.js";
+import { fakerPT_BR as faker } from "@faker-js/faker";
+import { hash } from "argon2";
+
+const passwordHash = await hash('748596')
+
+export async function seed() {
+   const insertUser = await db.insert(users).values({
+    name: faker.person.fullName(),
+    email: faker.internet.email().toLowerCase(),
+    phone: faker.phone.number(),
+    password: passwordHash,
+    role: 'user'
+   }).returning()
+
+   return insertUser[0]
+}
+
+seed()
