be779aab7c9167f57a94da40d5526fe85ed29384
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-07 11:36:58 -0300

    feat: add jsonwebtoken and return token and error message

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -4,6 +4,7 @@ import { users } from "@/db/schema.js";
 import { db } from "@/db/index.js";
 import { eq } from "drizzle-orm";
 import { verify } from "argon2";
+import jwt from 'jsonwebtoken'
 
 export const authRouter: FastifyPluginAsyncZod = async (app) => {
     app.post('/login', {
@@ -15,7 +16,7 @@ export const authRouter: FastifyPluginAsyncZod = async (app) => {
                 password: z.string()
             }),
             response: {
-                200: z.object({ message: z.string()}),
+                200: z.object({ token: z.string()}),
                 400: z.object({ error: z.string()})
             }
         }
@@ -27,7 +28,7 @@ export const authRouter: FastifyPluginAsyncZod = async (app) => {
        .where(eq(users.email, email))
 
        if (result.length === 0)  {
-        return reply.status(400).send({ error: 'Credencias inválidas.'})
+        return reply.status(400).send({ error: 'Credencias inválidas, verifique se o email ou senha estao corretos.'})
        }
 
        const user = result[0]
@@ -35,10 +36,16 @@ export const authRouter: FastifyPluginAsyncZod = async (app) => {
        const verifyPassword = await verify(user.password, password)
 
        if (!verifyPassword) {
-        return reply.status(400).send({ error: 'Credencias inválidas.'})
+        return reply.status(400).send({ error: 'Credencias inválidas, verifique se o email ou senha estao corretos.'})
        }
 
-       return reply.status(200).send({ message: 'Usuario validado'})
+       if(!process.env.JWT_SECRET) {
+        throw new Error('JWT_SECRET precisa ser setado.')
+       }
+
+       const token = jwt.sign({ sub: user.id, role: user.role}, process.env.JWT_SECRET)
+
+       return reply.status(200).send({ token })
        
     } )
 }
