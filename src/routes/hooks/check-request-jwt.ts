29a336ec8add58f7e0f26225231ee6d19ba6ab2a
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-07 11:38:40 -0300

    add authentication and role checking middleware

---
+++ src/routes/hooks/check-request-jwt.ts
@@ -0,0 +1,34 @@
+import type { FastifyRequest, FastifyReply } from "fastify";
+import jwt  from 'jsonwebtoken'
+
+type JwtPayload = {
+    sub: string
+    role: 'manager' | 'user'
+}
+
+export async function checkAutentication(request: FastifyRequest, reply: FastifyReply) {
+    const authHeader = request.headers.authorization
+
+    if (!authHeader) {
+        return reply.status(401).send({ error: 'Nao autorizado' })
+    }
+
+    const token = authHeader.startsWith('Bearer ')
+        ? authHeader.slice(7)
+        : authHeader
+
+    if (!process.env.JWT_SECRET) {
+        throw new Error('JWT_SECRET precisa ser setado.')
+    }
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload
+        
+        request.user = payload
+
+    } catch (error) {
+        return reply.status(401).send('Nao autorizado')
+    }
+
+
+}
---
+++ src/routes/hooks/check-use-role.ts
@@ -0,0 +1,12 @@
+import { getAuthUserReq } from "@/utils/getAuthUser.js";
+import type { FastifyRequest, FastifyReply } from "fastify";
+
+export function checkUserRole(role: 'manager' | 'user') {
+    return async function (request: FastifyRequest, reply: FastifyReply) {
+        const user = getAuthUserReq(request)
+
+        if (user.role !== role) {
+            return reply.status(401).send()
+        }
+    }
+}
