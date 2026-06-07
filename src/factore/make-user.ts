3b9c8194c451ba8d718b7f5400882cb8535d8dd0
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-07 11:30:43 -0300

    feat: add function create user and job to make tests

---
+++ src/factore/make-user.ts
@@ -0,0 +1,31 @@
+import { users } from "@/db/schema.js";
+import { db } from "@/db/index.js";
+import { fakerPT_BR as faker } from "@faker-js/faker";
+import { hash } from "argon2";
+import jwt from 'jsonwebtoken'
+
+export async function makeUser(role?: 'manager' | 'user') {
+   
+    const passwordHash = 'dev4837'
+
+    const insertUser = await db.insert(users).values({
+        name: faker.person.fullName(),
+        email: faker.internet.email().toLowerCase(),
+        phone: faker.phone.number(),
+        password: await hash(passwordHash),
+        role: role
+    }).returning()
+
+    return {
+        user: insertUser[0],
+        passwordHash,
+    }
+}
+
+export async function authenticationUser(role: 'manager' | 'user') {
+    const { user, passwordHash } = await makeUser(role)
+
+    const token = jwt.sign( { sub: user.id, role: user.role }, process.env.JWT_SECRET!)
+
+    return { user, token, passwordHash }
+}
