87846fc5f83ebe38724473cab3c94220a3e6e4fe
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-08 17:06:16 -0300

    refactor: remove unused startWhatsaap worker function

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import { server } from './app.js'
 import { startProcessor } from './modules/whatsapp/whatsapp.processor.js'
-import { startWhatsappWorker } from './modules/whatsapp/whatsaap.worker.js'
 
-startWhatsappWorker()
 startProcessor()
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
