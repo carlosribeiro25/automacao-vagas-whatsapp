c6388dba9524bdcc02d85ca74c92d3d77228e44a
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-08 10:50:35 -0300

    feat: add routes to reset new password

---
+++ src/services/email.service.ts
@@ -0,0 +1,15 @@
+import { Resend } from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function sendResetEmail(email: string, token: string) {
+  const link = `${process.env.FRONTEND_URL}/reset-password?token=${token}`
+
+  await resend.emails.send({
+    from: 'noreply@wilsoncarlostech.uk',
+    to: email,
+    subject: 'Recuperação de senha',
+    html: `<p>Clique no link para recuperar sua senha.Token válido por 15 minutos.</p>
+        <a href="${link}"> ${link} </a>`
+  })
+}
